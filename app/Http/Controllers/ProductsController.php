2d68ec363dc0d8edfbf48507f40b01f774b465ab
JuraganPacarCina <145534732+takumifahri@users.noreply.github.com>
2025-02-04 13:41:56 +0700

    Add Auth with Breeze

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
@@ -9,9 +10,12 @@ class ProductsController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        // $products = Product::query()->get();
+        $products = new Product();
+        $products = $products->query()->get();
+        return view('product', ['products' => $products]);
     }
 
     /**
@@ -19,7 +23,7 @@ class ProductsController extends Controller
      */
     public function create()
     {
-        //
+        // 
     }
 
     /**
@@ -28,6 +32,28 @@ class ProductsController extends Controller
     public function store(Request $request)
     {
         //
+        $products = new Product();
+        $request->validate([
+            'product_id' => 'required',
+            'nama_produk' => 'required',
+            'harga' => 'required',
+            'stok' => 'required',
+            'deskripsi' => 'required',
+            'gambar' => 'required'
+        ]);
+        try{
+            $products->product_id = $request->product_id;
+            $products->nama_produk = $request->nama_produk;
+            $products->harga = $request->harga;
+            $products->stok = $request->stok;
+            $products->deskripsi = $request->deskripsi;
+            $products->gambar = $request->gambar;
+            $products->save();
+            return redirect()->route('produk')->with('success', 'Data berhasil ditambahkan');
+        }
+        catch(\Exception $e){
+            return redirect()->route('produk')->with('error', 'Data gagal ditambahkan');
+        }
     }
 
     /**
